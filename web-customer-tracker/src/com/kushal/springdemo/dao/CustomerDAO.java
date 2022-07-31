d49b2f46473aef8a6cb8c2565821b58bc9608c21
Kushal <kushalsha133@gmail.com>
2022-07-31 11:46:43 +0530

    RemovedRedundantFiles

--- web-customer-tracker/src/com/kushal/springdemo/dao/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.kushal.springdemo.dao;
-
-import java.util.List;
-
-import com.kushal.springdemo.entity.Customer;
-
-public interface CustomerDAO {
-	public List<Customer> getCustomers();
-}
