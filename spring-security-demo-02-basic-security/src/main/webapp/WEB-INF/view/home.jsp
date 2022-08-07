c9802a7ba314d1508432018e2ea69d35d60bb45d
Kushal <kushalsha133@gmail.com>
2022-08-07 13:48:35 +0530

    SpringSecurity Custom Login

---
+++ spring-security-demo-02-basic-security/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Kush Company Home Page</title>
+</head>
+<body>
+<h2>Kushal Company Home Page</h2>
+<hr>
+Welome to our Home Page
+</body>
+</html>
---
+++ spring-security-demo-03-custom-login-form/src/main/java/com/kushal/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.kushal.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	 //this path was specified in securityConfig
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoggingPage() {
+		return "plain-login";
+	}
+}
